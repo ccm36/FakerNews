09aec0ca5dab3b71850ce490104a696adc49ebb7
ccm36 <cameron.c.malloy@gmail.com>
2018-03-21 15:47:42 -0500

    Implement signup and login mutations

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,22 +1,47 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET } = require('../../config')
+
 function post(parent, args, context, info) {
   const { url, description } = args
   return context.db.mutation.createLink({ data: { url, description } }, info)
 }
 
-function signup(parent, args, context, info) {
-  const { name, email, password } = args
-  return context.db.mutation.createUser({ data: { name, email, password } }, info)
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10)
+  const user = await context.db.mutation.createUser({
+    data: { ...args, password },
+  })
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
 }
 
-function login(parent, args, context, info) {
-  const { email, password } = args
-  // compare hashed password to login password
-    // if correct,
-      // generate and return JWT and user info 
+async function login(parent, args, context, info) {
+  const user = await context.db.query.user({ where: { email: args.email } })
+  if (!user) {
+    throw new Error(`Could not find the user with email: ${args.email}`)
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
 }
 
 module.exports = {
   post,
   signup,
-  login,
+  login
 }
