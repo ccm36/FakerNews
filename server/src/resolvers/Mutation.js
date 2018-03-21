78c343936032bd11601b9fa65d37a48b6401a5e9
ccm36 <cameron.c.malloy@gmail.com>
2018-03-21 15:17:51 -0500

    Add User to data model and Prisma schema

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -3,6 +3,20 @@ function post(parent, args, context, info) {
   return context.db.mutation.createLink({ data: { url, description } }, info)
 }
 
+function signup(parent, args, context, info) {
+  const { name, email, password } = args
+  return context.db.mutation.createUser({ data: { name, email, password } }, info)
+}
+
+function login(parent, args, context, info) {
+  const { email, password } = args
+  // compare hashed password to login password
+    // if correct,
+      // generate and return JWT and user info 
+}
+
 module.exports = {
   post,
+  signup,
+  login,
 }
