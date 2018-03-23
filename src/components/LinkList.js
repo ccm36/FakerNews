5f8ea9792004178f5cf575804353e7b524199fa8
ccm36 <cameron.c.malloy@gmail.com>
2018-03-23 14:46:35 -0500

    Implement graphql query and render links to page

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import Link from './Link'
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'My Portfolio Site',
-        url: 'http://cammalloy.com',
-      },
-      {
-        id: '2',
-        description: 'Oh wow, cool site!',
-        url: 'https://www.reddit.com',
-      },
-    ]
+    console.log(this.props.feedQuery)
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading...</div>
+    }
+
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.feedQuery.feed.links
 
     return (
       <div>
@@ -24,4 +24,19 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList
+// Query sent to the server, returns data as a prop for the LinkList Component
+  // because of the optionally passed second argument in graphql (i.e. name)
+  // you will reference 'feedQuery' as the prop name (otherwise defaults to 'data')th
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList)
