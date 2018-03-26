4a2a515b510ed35eca3aa26c22360466f7c5f359
ccm36 <cameron.c.malloy@gmail.com>
2018-03-26 17:18:22 -0500

    Begin implementation of authenticated Login/Signup user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import { AUTH_TOKEN } from '../constants'
 
 class Header extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN)
     return (
       <div className="flex pa1 justify-between nowrap green">
         <div className="flex flex-fixed yellow">
@@ -11,10 +13,34 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline yellow">
             new
           </Link>
-          <div className="ml1">|</div>
-          <Link to="/create" className="ml1 no-underline yellow">
-            submit
-          </Link>
+          {authToken && (
+            <div className="flex">
+              <div className="ml1">|</div>
+              <Link to="/create" className="ml1 no-underline yellow">
+                submit
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-fixed">
+          {authToken
+            ? (
+                <div
+                  className="ml1 pointer yellow"
+                  onClick={() => {
+                    localStorage.removeItem(AUTH_TOKEN)
+                    this.props.history.push('/')
+                  }}
+                >
+                  logout
+                </div>
+              )
+            : (
+                <Link to="/login" className="ml1 no-underline yellow">
+                  login
+                </Link>
+              )
+          }
         </div>
       </div>
     )
