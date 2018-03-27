f07cef4c178674b6116ae7c6dc3629464f5a1dcf
ccm36 <cameron.c.malloy@gmail.com>
2018-03-27 15:18:19 -0500

    Search component fetches the queried results from db

--- src/components/Search.js
+++ src/components/Search.js
@@ -36,8 +36,44 @@ class Search extends Component {
   }
 
   _executeSearch = async () => {
-    // TODO
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
   }
 }
 
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
 export default withApollo(Search)
+// This is so we can load the data everytime the user hits 'search'
+  // not upon initial load of the component
+// withApollo injects the ApolloClient instance (from index.js)
+// into the Search component as a new prop, called 'client'
+  // client has a method called 'query'
+    // allows you to manually send a query instead of
+    // using the 'graphql' HOC
